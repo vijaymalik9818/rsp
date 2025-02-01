daa666cf6011fd0a73b42cebf132b3ee9f419348
peregrine-it <siddharth@peregrine-it.com>
2025-02-01 14:42:44 +0530

    Change in the cron

--- app/Http/Controllers/Apis/AgentController.php
+++ app/Http/Controllers/Apis/AgentController.php
@@ -1440,6 +1440,7 @@ public function getadvanceData(Request $request)
                 'BedroomsTotal',
                 'ListAgentFullName',
                 'ListOfficeName',
+                'ListOfficePhone',
                 'BuildingAreaTotalSF',
                 DB::raw("CASE 
                                 WHEN `PropertySubtype` IN ('Apartment', 'Row/Townhouse') 
